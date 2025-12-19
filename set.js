7e1845608e79fc5acf79b56daa0e3dfc8fd82227
Nikhil PP <nikhilpp@Nikhils-MacBook-Air.local>
2025-12-19 16:27:18 +0530

    new map

--- set.js
+++ set.js
@@ -1,4 +1,4 @@
-// set
+// set and map
 const newMap = new Map();
 newMap.set('a',[1,2,3]);
 newMap.set('b',2);
@@ -8,3 +8,6 @@ console.log('newMap', newMap.size);
 console.log('newMap', newMap.has('a'));
 console.log('newMap', newMap.delete('a'));
 console.log('newMap', newMap);
+const newSet = [1,2,3,4,4,5,5,5,6];
+const uniqueSet = [...new Set(newSet)];
+console.log('uniqueSet',uniqueSet)
